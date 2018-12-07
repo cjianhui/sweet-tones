e67b00da3c8c9d93d548d46fbcdcc6010dcdecdf
cjianhui <cjianhui1993@gmail.com>
2018-12-08 02:51:22 +0800

    Added resolvers and mutations for graphql

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: check if user logged in
+
+        const item = ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+};
 
 module.exports = mutations;
