6b5493616b226818ea746a331f98d5be19551224
cjianhui <cjianhui1993@gmail.com>
2018-12-17 21:20:34 +0800

    Added image upload function via Cloudinary API

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -32,8 +32,8 @@ class CreateItem extends Component {
     state = {
         title: 'Cool Shoes',
         description: 'I love those shoes',
-        image: '',
-        largeImage: '',
+        image: 'dog.jpg',
+        largeImage: 'large-dog.jpg',
         price: 1000,
     };
 
@@ -41,14 +41,34 @@ class CreateItem extends Component {
         const {name, type, value} = e.target;
         const val = type == 'number' ? parseFloat(value) :
             value;
-        this.setState({[name]: e.target.value});
+        this.setState({[name]: val});
+    };
+
+    uploadFile = async e => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sweet-tones');
+
+        const res = await fetch
+        ('https://api.cloudinary.com/v1_1/cjianhui/image/upload',
+            {
+                method: 'POST',
+                body: data
+            });
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        });
     };
 
     render() {
         return (
             <Mutation mutation={CREATE_ITEM_MUTATION} variables=
                 {this.state}>
-                {(createItem, {loading, error}) => (
+                {(createItem, { loading, error }) => (
                     <Form onSubmit={async e => {
                         // stop form from submitting
                         e.preventDefault();
@@ -63,6 +83,19 @@ class CreateItem extends Component {
                     }>
                         <Error error={error} />
                         <fieldset disabled={loading} aria-busy={loading}>
+                            <label htmlFor="file">
+                                Image
+                                <input type="file"
+                                       id="file"
+                                       name="file"
+                                       placeholder="Upload an image"
+                                       required
+                                       onChange={this.uploadFile}
+                                />
+                                {this.state.image && <img src={this.state.image} alt="Upload Preview"/>}
+                            </label>
+
+
                             <label htmlFor="title">
                                 Title
                                 <input type="text"
@@ -109,4 +142,4 @@ class CreateItem extends Component {
 }
 
 export default CreateItem;
-export {CREATE_ITEM_MUTATION};
+export { CREATE_ITEM_MUTATION };
